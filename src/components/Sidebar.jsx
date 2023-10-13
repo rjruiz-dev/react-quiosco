ad848caec76a5342fdf32577d0241db7a9a4c6c2
rjruiz <rjruizsf@gmail.com>
2023-10-13 15:42:06 -0300

    Creando el layout principal

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+export default function Sidebar() {
+    return (
+        <div className="md:w-72">Sidebar</div>
+    )
+} 
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,24 @@
+// Dependencias o Hooks
+
+// Componentes
 import { Outlet } from 'react-router-dom' // Para visualizar el hijo
+import  Sidebar  from '../components/Sidebar'
+import  Resumen  from '../components/Resumen'
+
+// Funciones
+
+// Hojas de estilo 
 
 export default function Layout() {
     return (
-        <div>
-            Layout
-            <Outlet />
+        <div className='md:flex'> {/* de izq a der tamaño mediano */}
+            <Sidebar />
+
+            <main className='flex-1'>
+                <Outlet /> {/* las rutas que vamos definiendo */}
+            </main>
+
+            <Resumen />
         </div>
     )
 }
