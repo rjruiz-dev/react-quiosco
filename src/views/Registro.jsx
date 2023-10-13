9878b59a06c09f44bf7af87d877600e124405f9f
rjruiz <rjruizsf@gmail.com>
2023-10-13 14:57:36 -0300

    Finalizando el formulario de registro

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -19,6 +19,55 @@ export default function Registro() {
                             placeholder="Tu nombre" 
                         />
                     </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="email"
+                        >Email:</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="email"
+                            placeholder="Tu email" 
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="password"
+                        >Password:</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="password"
+                            placeholder="Tu password" 
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="password_confirmation"
+                        >Repetir Password:</label>
+                        <input 
+                            type="password"
+                            id="password_confirmation"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="password_confirmation"
+                            placeholder="Repetir password" 
+                        />
+                    </div>
+
+                    <input 
+                        type="submit"
+                        value="Crear cuenta"
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 
+                        p-3 uppercase font-bold cursor-pointer"
+                    />
                 </form>
             </div>
         </>
