5676697bd6c83c8e7a136d20f48f01ee8870cd0c
rjruiz <rjruizsf@gmail.com>
2023-10-13 14:47:00 -0300

    Primeros pasos con el formulario de registro

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,5 +1,26 @@
 export default function Registro() {
     return (
-        <div>Registro</div>
+        <>
+            <h1 className="text-4xl font-black">Crea tu cuenta</h1>
+            <p>Crea tu Cuenta llenando el formulario</p>
+
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form action="">
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="name"
+                        >Nombre:</label>
+                        <input 
+                            type="text"
+                            id="name"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="name"
+                            placeholder="Tu nombre" 
+                        />
+                    </div>
+                </form>
+            </div>
+        </>
     );
 }
