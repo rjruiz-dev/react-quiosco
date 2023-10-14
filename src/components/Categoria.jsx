20a6e169289527a3b79bb27b0e478fe435029ef8
rjruiz <rjruizsf@gmail.com>
2023-10-14 14:53:05 -0300

    Props comunicando desde Sidebar hacia Categoria

---
+++ src/components/Categoria.jsx
@@ -0,0 +1,12 @@
+
+export default function Categoria(props) {
+    
+    // aqui cualquier codigo,logica de javascript 
+    console.log(props)
+    
+    // aqui lo que queremos representar
+    return (
+        <div>Categoria</div>
+    );
+}
+
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { categorias } from "../data/categoria"
+import Categoria from "../components/Categoria"
 
 export default function Sidebar() {
+    // Lo que este dentro del return es lo que se muestra en pantalla
     return (
         <aside className="md:w-72">
             <div className="p-4">
@@ -10,10 +12,17 @@ export default function Sidebar() {
                 />
             </div>
 
+            {/* Comunicamos desde Sidebar hacia Categoria */}
             <div className="mt-10">
-                {categorias.map( categorias => (
-                    <p>{categorias.nombre}</p>
-                ))} {/* genera un nuevo arreglo para mostrar en pantalla */}
+                {/* categorias.map genera un nuevo arreglo para mostrar en pantalla */}
+                {categorias.map( categoria => (
+                    // <p>{categorias.nombre}</p>
+                    <Categoria 
+                        // pasar el obj categorias al componente Categoria a traves de props (argumentos)
+                        // se pueden pasar: string, booleanos, array, funciones
+                        categoria={categoria}
+                    />
+                ))} 
             </div>
         </aside>
     )
