f7315cb3819346d001ec62a75b2309a9d9f777a4
rjruiz <rjruizsf@gmail.com>
2023-10-16 15:29:15 -0300

    Obteniendo los productos

---
+++ src/components/Producto.jsx
@@ -0,0 +1,7 @@
+export default function Producto({producto}) {
+
+    console.log(producto)
+    return (
+        <div>Producto</div>
+    )
+}
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,5 +1,26 @@
+import { productos } from '../data/productos'
+import Producto from "../components/Producto"
+
 export default function Inicio() {
+
+    console.log(productos)
     return (
-        <div>Inicio</div>
+        
+        // <> </> fragment
+        <>
+            <h1 className='text-4xl font-black'>Inicio</h1>
+            <p className='text-2xl my-10'>
+                Elige y personaliza tu pedio a continuación.
+            </p>
+            {/* display de grid a los elem hijos */}
+            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+                {productos.map(producto => (
+                    <Producto 
+                        key={producto.imagen}
+                        producto={producto}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
