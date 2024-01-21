607c3387bf89a53a235b9824245e0b06d8e61533
rjruiz <rjruizsf@gmail.com>
2024-01-21 10:24:20 -0300

    Añadiendo la posibilidad de ajustar las cantidades

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react" // viene definida en react
 import useQuiosco from "../hooks/useQuiosco" // Importar el hook para tener acceso al producto
 import { formatearDinero } from "../helpers"
 
@@ -5,9 +6,10 @@ import { formatearDinero } from "../helpers"
 export default function ModalProducto(){
 
     const { producto, handleClickModal } = useQuiosco();
+    const [cantidad, setCantidad] = useState(1)
     
     return (
-        <div className="md:flex gap-10">
+        <div className="md:flex items-center gap-10">
            {/* Del lado izq la imagen y del lado der la informacion  */}
             <div className="md:w-1/3"> {/*1 de 3 columnas*/}
                 <img 
@@ -32,7 +34,36 @@ export default function ModalProducto(){
                     {/* {producto.precio} */}
                     {formatearDinero(producto.precio)}
                 </p>
+                <div className="flex gap-4 mt-5">
+                    {/* boton - */}
+                    <button
+                        type="button"
+                        onClick={() => {
+                            // solo se pueden agregar hasta 5 productos
+                            if(cantidad <= 1) return
+                            setCantidad(cantidad - 1);
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </button>
 
+                    <p className="text-3xl">{cantidad}</p>
+                    {/* boton + */}
+                    <button
+                        type="button"
+                        onClick={() => {
+                            // solo se pueden agregar hasta 5 productos
+                            if(cantidad >= 5) return
+                            setCantidad(cantidad + 1);
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </button>
+                </div>
                 <button 
                     type="button"
                     className="bg-indigo-600 hover:bg-indigo-800 px-5 py-5 mt-5 text-white
