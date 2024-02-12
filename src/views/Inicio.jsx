2454fbe5ce39910313ecd101644eeed7719970b2
rjruiz <rjruizsf@gmail.com>
2024-02-12 13:56:39 -0300

    Instalacion de swr para el estado del servidor y revalidacion de la informacion

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,14 +1,30 @@
-import { productos as data} from '../data/productos'
+// import { productos as data} from '../data/productos'
+import useSWR from 'swr'
 import Producto from "../components/Producto"
 import useQuiosco from '../hooks/useQuiosco';
+import clienteAxios from '../config/axios';
 
 export default function Inicio() {
 
     const { categoriaActual } = useQuiosco()
     // console.log(categoriaActual)
 
+    // consulta SWR
+    const fetcher = () => clienteAxios('/api/productos').then(data => // console.log(data)
+        data.data
+    )
+    
+    const {data, error, isLoading} = useSWR('/api/productos', fetcher, {
+        refreshInterval: 1000 // 1seg
+    })
+    // console.log(data)
+    // console.log(error)
+    // console.log(isLoading)
+    if(isLoading) return 'Cargando...'
+    // return
+
     // console.log(productos)
-    const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+    const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
     return (
         
         // <> </> fragment
