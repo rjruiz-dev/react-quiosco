1ca085659831bc1c24a274c8508bf5d7a684d5f6
rjruiz <rjruizsf@gmail.com>
2024-02-15 15:49:09 -0300

    Habilitando CORS en laravel para que acepte peticiones de react

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,6 +3,12 @@ import axios from "axios";
 // crear el cliente axios siempre que llame esta lib busca esta config
 const clienteAxios = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    // headers: se envian antes de la peticion
+    headers: {
+        'Accept' : 'application/json',
+        'X-Requested-With' : 'XMLHttpRequest'
+    },
+    withCredentials: true
 })
 
 export default clienteAxios
